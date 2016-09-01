ab2900475652411f5bfbd7e90f72cfe6f872041d
ranog <jprnogueira@yahoo.com.br>
2016-09-01 15:53:22 -0300

    home.html usando sistema de herança do base.html

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from core import views
 
 urlpatterns = [
-            url(r'^$', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
+    url(r'^contact/$', views.contact, name='contact'),
     url(r'^admin/', admin.site.urls),
 ]
